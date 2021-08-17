19d0ae8c11b47d96d23b03d1ed5dab6ddb1c2296
Mateusz Budzisz <mat.budzisz@gmail.com>
2021-08-17 15:39:03 +0200

    applied crud

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,42 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    # strong params
+    # prevents adding code that could mess with a program and db
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks # this is a shortcut for all 7 common routes
+  # get 'tasks/new',      to: "tasks#new",      as: :new
+  # post "tasks",         to: "tasks#create"
+  # get 'tasks',          to: "tasks#index"
+  # get "tasks/:id",      to: "tasks#show",     as: :task #show is name used to target items by id
+  # get "tasks/:id/edit", to: "tasks#edit",     as: :edit
+  # patch "tasks/:id/",   to: "tasks#update"
+  # delete "tasks/:id",   to: "tasks#destroy"
 end
